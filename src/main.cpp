61618f596a4988154fe31d715cd8c6602a077617
Quentin Horgues <quentin.horgues@outlook.fr>
2024-02-06 19:23:51 +0100

    modification matrice

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
+#include <Matrix.hpp>
 
 int main()
-{    
+{
+    Matrix<int, 3> A {
+        {
+            {1, 3, 8},
+            {8, 3, 2},
+            {0, 8, 1}
+        }
+    };
+    std::cout << A << std::endl;
 }
