90240f8624dc6b44d2167693e67ff23622a3ca69
Maël Petit <mael2004.petit@gmail.com>
2024-02-09 15:50:54 +0100

    ajout enum piece

---
+++ src/GameBoard/Piece.hpp
@@ -0,0 +1,22 @@
+#ifndef PIECE_HPP
+#define PIECE_HPP
+
+enum class Piece
+{
+    W_KING,
+    W_BISHOP,
+    W_QUEEN,
+    W_ROOK,
+    W_KNIGHT,
+    W_PAWN,
+
+    B_KING,
+    B_BISHOP,
+    B_QUEEN,
+    B_ROOK,
+    B_KNIGHT,
+    B_PAWN,
+    EMPTY
+};
+
+#endif
