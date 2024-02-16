9bbb1671ec39a417b73b174dde6c8e87a7ef8d72
Maël Petit <mael2004.petit@gmail.com>
2024-02-16 14:41:40 +0100

    modification utilisation matrices

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,14 @@ namespace std
     std::string to_string(std::complex<T> value)
     {
         return std::string{std::to_string(std::real(value)) 
-                            + ((std::imag(value) > 0) ? '+' : '-') 
+                            + ((std::imag(value) >= 0) ? '+' : '-') 
                             + std::to_string(std::imag(value)) + 'i'};
     }
 
     template <std::floating_point T>
     std::ostream& operator<<(std::ostream& os, std::complex<T> value)
     {
-        os << std::real(value) << ((std::imag(value) > 0) ? '+' : '-') << std::imag(value) << 'i';
+        os << std::real(value) << ((std::imag(value) >= 0) ? '+' : '-') << std::imag(value) << 'i';
         return os;
     }
 }
