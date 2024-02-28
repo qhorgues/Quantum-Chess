fecee06aae087a12c8c0c1cd21a7557ce9d1b94c
Quentin Horgues <quentin.horgues@outlook.fr>
2024-02-28 16:15:57 +0100

    adaptation des testes aux nouvelles représentations

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,17 @@
 #include <CMatrix.hpp>
 #include <Unitary.hpp>
 #include <Qubit.hpp>
-#include <GameBoard.hpp>
+#include <Board.hpp>
+#include <Piece.hpp>
+#include <observer_ptr.hpp>
+#include <check_path.hpp>
 
 int main()
 {
-    using enum Piece;
     Board<2> B {
         {
-            {W_KING, B_KING},
-            {EMPTY, W_QUEEN}
+            { make_observer(&W_QUEEN),  make_observer(&B_KING)},
+            {                 nullptr, make_observer(&B_QUEEN)}
         }
     };
     Qubit<2> q {std::array<bool, 2> {false, true}};
@@ -23,17 +25,17 @@ int main()
 
     Board<4> B2 {
         {
-            {W_QUEEN, EMPTY, EMPTY, B_QUEEN},
-            {EMPTY, EMPTY, B_PAWN, W_KING},
-            {W_PAWN, EMPTY, EMPTY, W_BISHOP},
-            {B_BISHOP, W_ROOK, EMPTY, B_KING}
+            {  make_observer(&W_QUEEN),                nullptr,                nullptr,   make_observer(&B_QUEEN) },
+            {                  nullptr,                nullptr, make_observer(&B_PAWN),    make_observer(&W_KING) },
+            {  make_observer (&W_PAWN),                nullptr,                nullptr, make_observer (&W_BISHOP) },
+            { make_observer(&B_BISHOP), make_observer(&W_ROOK),                nullptr,    make_observer(&B_KING) }
         }
     };
-    B2.move_split_jump(B2.offset(0, 0), B2.offset(0,1), B2.offset(2,3));
-    std::cout << std::boolalpha << B2.mesure(B2.offset(2,3)) << std::endl;
-    B2.move_classic_jump(8, 13);
-    std::cout << std::boolalpha << B2.check_path_straight(Coord(0, 0), Coord(3, 0)) << std::endl;
-    std::cout << std::boolalpha << B2.check_path_diagonal(Coord(0, 0), Coord(3, 3)) << std::endl;
+    B2.move_split_jump(Coord(0, 0), Coord(0,1), Coord(2,2));
+    std::cout << std::boolalpha << check_path_straight(B2, Coord(0, 0), Coord(3, 0)) << std::endl;
+    B2.move_merge_jump(Coord(0,1), Coord(2, 2), Coord(0, 0));
+    std::cout << std::boolalpha << check_path_diagonal(B2, Coord(0, 0), Coord(3, 3)) << std::endl;
+    B2.move_classic_slide(Coord(0,3), Coord(3, 3), &check_path_straight_1_instance<4, 4>);
 
     return 0;
 }
