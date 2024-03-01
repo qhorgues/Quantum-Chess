88253c5bf65c4158aa93200f693bcee74684b294
Quentin Horgues <77660464+qhorgues@users.noreply.github.com>
2024-03-01 17:16:31 +0100

    ajout fonction math

---
+++ src/math_utility/math_utility.hpp
@@ -0,0 +1,7 @@
+#ifndef MATH_UTILITY_HPP
+#define MATH_UTILITY_HPP
+
+bool double_equal(double x, double y);
+bool complex_equal(std::complex<double> const &z1, std::complex<double> const &z2);
+
+#endif
