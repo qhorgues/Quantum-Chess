4729133c037789ebe950855b1574b744e5d69599
Quentin Horgues <quentin.horgues@outlook.fr>
2024-05-25 13:42:07 +0200

    affichage type piece

--- src/main.cpp
+++ src/main.cpp
@@ -15,32 +15,83 @@
 #include <ComputerPlayer.hpp>
 
 template <std::size_t N, std::size_t M>
-void auto_playing(Board<N, M> &board, std::ostream &output, int nb_moves, int search_depth)
+char print_piece(Board<N, M> &board, Coord const& position)
 {
-    while (nb_moves > 0 && !board.winning_position(opponent_color(board.get_current_player())))
+    TypePiece piece { board(position.n, position.m).get_type() };
+    Color color { board(position.n, position.m).get_color() };
+    using enum TypePiece;
+    
+    int const offset { (color == Color::BLACK) ? 32 : 0 };
+    using namespace std::literals;
+    switch (piece)
+    {
+    case KING:
+        return static_cast<char>('K' + offset);
+    case QUEEN:
+        return static_cast<char>('Q' + offset);
+    case ROOK:
+        return static_cast<char>('R' + offset);
+    case BISHOP:
+        return static_cast<char>('B' + offset);
+    case KNIGHT:
+        return static_cast<char>('N' + offset);
+    case PAWN:
+        return static_cast<char>('P' + offset);
+    case EMPTY:
+        return ' ';
+    default:
+        return '\0';
+    }
+}
+
+template <std::size_t N, std::size_t M>
+void auto_playing(
+    Board<N, M> &board, 
+    std::ostream &output, 
+    int nb_moves, 
+    int search_depth)
+{
+    while (
+        nb_moves > 0 &&
+         !board.winning_position(opponent_color(board.get_current_player())))
     {
 
         Move m{computer::get_best_move(board, search_depth)};
-        std::string data1{{static_cast<char>('a' + m.normal.src.m), static_cast<char>('0' + N - m.normal.src.n)}};
-        std::string data2{{static_cast<char>('a' + m.normal.arv.m), static_cast<char>('0' + N - m.normal.arv.n)}};
+
+        std::string data1{{
+            static_cast<char>('a' + m.normal.src.m), 
+            static_cast<char>('0' + N - m.normal.src.n)}};
+
+        std::string data2{{
+            static_cast<char>('a' + m.normal.arv.m), 
+            static_cast<char>('0' + N - m.normal.arv.n)}};
+            
+        output << print_piece(board, m.normal.src) << ' ';
         if (m.type == TypeMove::NORMAL)
         {
             output << "N " << data1 << data2 << std::endl;
         }
         else if (m.type == TypeMove::PROMOTE)
         {
-            output << "P " << data1 << data2 << " :: " << std::to_string(static_cast<int>(m.promote.piece)) << std::endl;
+            output << "P " << data1 << data2 << " :: " 
+            << std::to_string(static_cast<int>(m.promote.piece)) 
+            << std::endl;
         }
         else
         {
-            std::string data3{{static_cast<char>('a' + m.split.arv2.m), static_cast<char>('0' + N - m.split.arv2.n)}};
+            std::string data3{{
+                static_cast<char>('a' + m.split.arv2.m), 
+                static_cast<char>('0' + N - m.split.arv2.n)}};
+
             if (m.type == TypeMove::SPLIT)
             {
-                output << "S " << data1 << '(' << data2 << data3 << ')' << std::endl;
+                output << "S " << data1 << '(' << data2 
+                << data3 << ')' << std::endl;
             }
             else
             {
-                output << "M " << '(' << data1 << data2 << ')' << data3 << std::endl;
+                output << "M " << '(' << data1 << data2 
+                << ')' << data3 << std::endl;
             }
         }
         board.move(m);
@@ -84,7 +135,7 @@ int main()
         {B_PAWN, Piece(), Piece()},
         {Piece(), W_QUEEN, W_KING}}; */
 
-    auto_playing(B3, std::cout, 15, 8);
+    auto_playing(ChessBoard, std::cout, 60, 5);
 
     // log_file.close();
     return 0;
