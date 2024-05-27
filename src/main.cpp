72e70760061de19cbc502a15c24db575174e80bd
Mael Petit <mael2004.petit@gmail.com>
2024-05-27 14:20:07 +0200

    ..

--- src/main.cpp
+++ src/main.cpp
@@ -25,14 +25,14 @@ int main()
          {Piece(), Piece(), W_QUEEN, W_KING}}};
 
          Board<4> Bt{
-        {{B_KING, Piece(), Piece(), Piece()},
+        {{B_KING, B_BISHOP, Piece(), Piece()},
          {Piece(), Piece(), Piece(), Piece()},
          {Piece(), Piece(),  Piece(), W_KNIGHT},
-         {Piece(), Piece(), Piece(), W_KING}}};
+         {Piece(), Piece(), W_ROOK, W_KING}}};
 
          Board<3> Bt1{
         {{B_KING, Piece(), Piece()},
-         {Piece(), B_ROOK, Piece()},
+         {Piece(), W_ROOK, Piece()},
          {Piece(), Piece(),  W_KING}}
          };
          Board<3> Bt2{
@@ -50,8 +50,8 @@ int main()
     /*bool b = Final::brut_force_quantum_chess(B2, 1, Color::WHITE);
     std::cout<<std::boolalpha<<b<<std::en
     dl;*/
-    auto[res, res2] {Final::res_pos(B, 2)};
+    auto[res, res2] {Final::res_pos(Bt1, 12)};
     std::cout<<res<<std::endl;
-    Final::print_stack<3>(res2);
+    Final::print_stack<4>(res2);
    
 }
