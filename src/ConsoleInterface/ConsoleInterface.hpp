2e5446e156588f139a8075037971edf088691694
Quentin Horgues <quentin.horgues@outlook.fr>
2024-06-01 11:55:33 +0200

    correction pour l'interface

--- src/ConsoleInterface/ConsoleInterface.hpp
+++ src/ConsoleInterface/ConsoleInterface.hpp
@@ -4,6 +4,14 @@
 #include <ostream>
 #include <Board.hpp>
 
+const char *getUnicodeChar(TypePiece piece, Color color);
+TypeMove chr_to_TypeMove(char type_move);
+char Piece_to_chr(TypePiece piece, Color color);
+Piece chr_to_Piece(char piece);
+
+template <std::size_t N, std::size_t M>
+void print_board_light(std::ostream &os, Board<N, M> const &board);
+
 template <std::size_t N, std::size_t M>
 std::ostream& operator<<(std::ostream& os, Board<N, M> const& board);
 
