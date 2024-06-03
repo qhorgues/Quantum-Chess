a187b736579c1a2c426907f5d3b7e8fdbd53ce9b
Quentin Horgues <quentin.horgues@outlook.fr>
2024-06-03 14:33:00 +0200

    retrait print

--- csv-to-graph.py
+++ csv-to-graph.py
@@ -10,14 +10,12 @@ with open('partie1.csv') as csv_file:
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
             heuristique.append(float(row[0]))
             move.append(row[1])
             print(f'\t{row[0]} / {row[1]}.')
             line_count += 1
-    print(f'Processed {line_count} lines.')
 
 x = np.arange(1, len(move)+1, 1)
 print(x)
